84a9a1807374e56c0fe9ce49f7e65d68ccf0d3e5
Omary Ally <omaryally.em@gmail.com>
2023-02-10 05:13:59 -0500

    connect db before listen to port

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 //todo - Declare variables
 const express = require("express");
 const app = express()
-const PORT = 8000
+const PORT = process.env.PORT
 const mongoose = require("mongoose")
 //*Import functions/routes
 const connectDB = require("./config/database")
 const homeRoutes = require('./routes/home')
 require('dotenv').config({path: './config/.env'})
 //todo - Connect to Database
-connectDB()
+connectDB().then(() => {
+  app.listen(PORT, () =>{
+    console.log(`Server running on port ${PORT}`);
+  })
+  })
 //todo - Set Middleware
 app.set("view engine", "ejs")
 app.set(express.static("public"))
@@ -18,6 +22,3 @@ app.use(express.urlencoded({extended: true}))
 app.use('/', homeRoutes)
 //todo - Start Server
 
-app.listen(PORT, () =>{
-  console.log(`Server running on port ${PORT}`)
-})
