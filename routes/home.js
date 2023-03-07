9cecda8df4a2de5cac8673ec1e4545576d74c6aa
Omary Ally <omaryally.em@gmail.com>
2023-03-06 23:40:27 -0500

    authentification

--- routes/home.js
+++ routes/home.js
@@ -4,8 +4,9 @@
 const express = require("express")
 const router = express.Router()
 const homeController = require('../controllers/home')
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
-router.get('/', homeController.getIndex)//read
+router.get('/',ensureAuth, homeController.getIndex)//read
 // router.get("/", homeController.getMember)
 router.post('/new', homeController.createMember)
 
